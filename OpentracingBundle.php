1e84d167fd8c880b6bd50315de462e147c954b5d
Christian Alexander Wolf <mail@cawolf.de>
2020-01-17 15:51:25 +0100

    add psr-18 support

--- OpentracingBundle.php
+++ OpentracingBundle.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Auxmoney\OpentracingBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-final class OpentracingBundle extends Bundle
+final class OpentracingBundle extends AbstractOpentracingBundle
 {
 }
