c9fdb275622c9cd3c639f8a06cd78d8d407782bb
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-04-30 11:26:32 +0200

    chore: functional tests from forks (#13)
    contributors: @cHeeSaW

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -2,5 +2,6 @@
 
 cd build/testproject/
 composer remove auxmoney/opentracing-bundle-jaeger
+composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
 composer require auxmoney/opentracing-bundle-jaeger:dev-${BRANCH}
 cd ../../
